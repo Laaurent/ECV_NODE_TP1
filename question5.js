c315e545cb96bad1d21de03b69053ac9f1aa6309
Laaurent <laurent-quenel@hotmail.Fr>
2021-12-17 13:33:02 +0100

    add error type

--- question5.js
+++ question5.js
@@ -3,8 +3,13 @@ var url = require("url");
 
 const server = http.createServer((req, res) => {
    res.writeHead(200, { "Content-Type": "text/Html" });
-   if (req.url == "/hello") content = "Hello, world!";
-   else content = "Not found!";
+   if (req.url == "/hello") {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      content = "Hello, world!";
+   } else {
+      res.writeHead(404, { "Content-Type": "application/json" });
+      content = "Not found!";
+   }
    res.write(content);
    res.end();
 });
